a6f8ce2fac2b632d0177ea8bb4fa1b88f464339c
Yumil Rueda Flores <yumil.ruedaflores@abim.tech>
2024-10-25 09:21:06 +0200

    init ex 3

---
+++ exercise.js
@@ -0,0 +1,25 @@
+/* *Instructions*
+    
+    There's somethign called falsy values in JavaScript and this exercise will help you in learning them.
+    
+    You will receive an array with all kinds of values and you need to remove all falsy values from the given array and
+    return the array with the remaining values.
+
+
+*/
+
+
+/** DO NOT CHANGE THE FUNCTION NAME **/
+const youShallNotPass = (arr) => {
+    /* Only make changes below this comment */
+    
+    
+    /* Only make changes below this comment */
+}
+
+
+
+
+
+/** DO NOT CHANGE THE LINE BELOW **/
+module.exports.youShallNotPass = youShallNotPass;
